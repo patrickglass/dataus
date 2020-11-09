ea209aed88689040206edfeb0c6f80926cd2e36f
Patrick Glass <patrick.glass@sap.com>
2020-11-08 21:18:58 -0800

    Initial Commit

---
+++ datus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  datus
+//
+//  Created by Glass, Patrick on 2020-11-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ datus/datusApp.swift
@@ -0,0 +1,17 @@
+//
+//  datusApp.swift
+//  datus
+//
+//  Created by Glass, Patrick on 2020-11-08.
+//
+
+import SwiftUI
+
+@main
+struct datusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
